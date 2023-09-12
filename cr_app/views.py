7fe8868a8d32e850163c834da73e58f6d9d7776d
Atharva <atharvad931@gmail.com>
2023-09-12 16:15:45 +0530

    Post request fixed

--- cr_app/views.py
+++ cr_app/views.py
@@ -6,6 +6,13 @@ def index(request):
     return render(request, 'index.html')
 
 def result(request):
+    print(request.body)
+    if(request.method == "POST"):
+        print('here')
+        context = request.POST.get('cards')
+        dummy_var = request.POST.get('dummy')
+        print(context)
+        print(dummy_var)
     return render(request, 'result.html', {
         'MU_result' : 3
     })
