b60d38c54bad5be7611e521996b2c110ba5eb197
Atharva <atharvad931@gmail.com>
2023-09-13 15:41:26 +0530

    ML model deployed

---
+++ cr_app/prediction.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pickle
+import os
+#total no. of cards 
+vector_max = 120    
+
+with open('cr_app/rf_cr_model.sav', 'rb') as file:
+    pickle_model = pickle.load(file)
+
+def predictionModel(cardList, oppCardList):
+    arr1 = np.asarray(cardList).reshape(1, -1)
+    arr2 = np.asarray(oppCardList).reshape(1, -1)
+    one_hot_wins = np.zeros((1, vector_max+1))
+    one_hot_losses = np.zeros((1, vector_max+1))
+    one_hot_wins[np.arange(1).reshape(1, -1), arr1]=1
+    one_hot_losses[np.arange(1).reshape(1, -1), arr2]=1
+    one_hot_arr = np.concatenate((one_hot_wins, one_hot_losses), axis=1)
+    # print(one_hot_arr.shape)
+    return pickle_model.predict(one_hot_arr)[0]
+
--- cr_app/views.py
+++ cr_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .prediction import predictionModel
 
 # Create your views here.
 def index(request):
@@ -7,12 +8,21 @@ def index(request):
 
 def result(request):
     print(request.body)
+    result = 0
     if(request.method == "POST"):
         print('here')
-        context = request.POST.get('cards')
-        dummy_var = request.POST.get('dummy')
-        print(context)
-        print(dummy_var)
+        cards = request.POST.get('cards')
+        opp_cards = request.POST.get('rcards')
+        print(cards)
+        print(opp_cards)
+        cards=cards.split(',')
+        opp_cards=opp_cards.split(',')
+        cards = [int(i) for i in cards]
+        opp_cards = [int(i) for i in cards]
+        print(cards)
+        result = predictionModel(cards, opp_cards)
     return render(request, 'result.html', {
-        'MU_result' : 3
+        'MU_result' : result
     })
+
+
