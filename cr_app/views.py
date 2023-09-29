3c2d1aeb3cc0f186c1f1cf797c290d40d1584a71
Atharva <atharvad931@gmail.com>
2023-09-29 18:56:20 +0530

    removed prints

--- cr_app/views.py
+++ cr_app/views.py
@@ -10,16 +10,14 @@ def result(request):
     print(request.body)
     result = 0
     if(request.method == "POST"):
-        print('here')
         cards = request.POST.get('cards')
         opp_cards = request.POST.get('rcards')
-        print(cards)
-        print(opp_cards)
         cards=cards.split(',')
         opp_cards=opp_cards.split(',')
         cards = [int(i) for i in cards]
-        opp_cards = [int(i) for i in cards]
+        opp_cards = [int(i) for i in opp_cards]
         print(cards)
+        print(opp_cards)
         result = predictionModel(cards, opp_cards)
     return render(request, 'result.html', {
         'MU_result' : result
