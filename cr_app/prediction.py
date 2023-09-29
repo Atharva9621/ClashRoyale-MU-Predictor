3301da71cb854b2be7f95f9b9398943c87cf8c55
Atharva <atharvad931@gmail.com>
2023-09-29 17:05:46 +0530

    added all cards

--- cr_app/prediction.py
+++ cr_app/prediction.py
@@ -6,7 +6,7 @@ vector_max = 120
 
 with open('cr_app/rf_cr_model.sav', 'rb') as file:
     pickle_model = pickle.load(file)
-
+    
 def predictionModel(cardList, oppCardList):
     arr1 = np.asarray(cardList).reshape(1, -1)
     arr2 = np.asarray(oppCardList).reshape(1, -1)
@@ -17,4 +17,3 @@ def predictionModel(cardList, oppCardList):
     one_hot_arr = np.concatenate((one_hot_wins, one_hot_losses), axis=1)
     # print(one_hot_arr.shape)
     return pickle_model.predict(one_hot_arr)[0]
-
